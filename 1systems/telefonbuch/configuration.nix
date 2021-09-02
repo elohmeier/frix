ed67821a6c5a90601dd275a06dc1d846f342de21
Enno Richter <enno.richter@fraam.de>
2021-09-02 07:44:47 +0200

    add telefonbuch host

---
+++ 1systems/telefonbuch/configuration.nix
@@ -0,0 +1,36 @@
+/*
+  Installation from booted NixOS ISO
+  ----------------------------------
+
+  1. Format disk according to 2configs/hetzner-vm.nix
+  2. Spawn Shell using `nix-shell -p nixFlakes gitMinimal`
+  3. Run `nixos-install --flake git+https://git.fraam.de/fraam/frix#telefonbuch --no-root-passwd --no-channel-copy`
+  4. Reboot
+
+*/
+
+{ config, pkgs, ... }:
+
+let
+  sshkeys = import ../../2configs/sshkeys.nix;
+in
+{
+  imports =
+    [
+      ../../default.nix
+      ../../2configs/hetzner-vm.nix
+    ];
+
+  networking.hostName = "telefonbuch";
+
+  systemd.network.networks."40-en".networkConfig = {
+    Address = "2a01:4f8:c0c:1992::1/64";
+  };
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    sshkeys.enno_yubi41
+    sshkeys.enno_yubi49
+  ];
+
+  system.stateVersion = "21.05";
+}
