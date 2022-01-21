745332b5741dd13a1a91eb542a397f03d803d2cf
Enno Richter <enno.richter@fraam.de>
2022-01-21 19:44:55 +0100

    metrics: fix log path

--- 1systems/metrics/traefik.nix
+++ 1systems/metrics/traefik.nix
@@ -9,7 +9,7 @@
     staticConfigOptions = {
       accessLog = {
         bufferingSize = 100;
-        filePath = "/var/lib/traefik/access.log.json";
+        filePath = "/var/log/traefik/access.log.json";
         format = "json";
         fields = {
           defaultMode = "keep";
