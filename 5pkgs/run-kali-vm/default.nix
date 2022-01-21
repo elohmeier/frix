309966fa69c8d2f8a5702ac06e4a437d761e8b3b
Enno Richter <enno.richter@fraam.de>
2022-01-21 10:36:43 +0100

    run-kali-vm: use virt-viewer

--- 5pkgs/run-kali-vm/default.nix
+++ 5pkgs/run-kali-vm/default.nix
@@ -1,25 +1,35 @@
-{ writeShellScriptBin, kali-vm-image }:
+{ writeShellScriptBin, kali-vm-image, qemu_kvm, virt-viewer }:
 
+# for integration:
+# 1. install spice-vdagent in kali vm using `sudo apt install spice-vdagent`
+# 2. reboot or start service manually
+# 3. resizing can be triggered by running `xrandr --output Virtual-1 --auto`
+# see also https://itectec.com/superuser/linux-no-auto-resize-with-spice-and-virt-manager/
 writeShellScriptBin "run-kali-vm" ''
   if [ ! -f kali-vm.qcow2 ]; then
     echo "generating kali-vm.qcow2 overlay image"
-    qemu-img create \
+    ${qemu_kvm}/bin/qemu-img create \
       -f qcow2 kali-vm.qcow2 80G \
       -b "${kali-vm-image}/kali-vm-image.qcow2" -F qcow2
   else
     echo "reusing existing kali-vm.qcow2 overlay image, delete that file if you need a fresh kali environment"
   fi
 
-  qemu-system-x86_64 \
+  ${qemu_kvm}/bin/qemu-system-x86_64 \
+    -daemonize \
     -nodefaults \
     -enable-kvm \
     -cpu host -smp 12 \
     -drive file=kali-vm.qcow2,if=virtio \
     -net nic -net user,hostname=kali-vm \
     -m 4G \
-    -monitor stdio \
-    -name kali-vm \
-    -device usb-ehci,id=ehci \
-    -device usb-tablet,bus=ehci.0 \
-    -vga std
+    -vga qxl \
+    -spice port=5924,disable-ticketing \
+    -usbdevice tablet \
+    -device virtio-serial \
+    -chardev spicevmc,id=vdagent,name=vdagent \
+    -device virtserialport,chardev=vdagent,name=com.redhat.spice.0 \
+    "$@"
+
+  ${virt-viewer}/bin/remote-viewer --title win-vm spice://127.0.0.1:5924
 ''
