de5b7a3e28e7596fee103cf797b68033441b6293
Enno Richter <enno.richter@fraam.de>
2022-02-28 14:07:57 +0100

    wv: initrd cfg

--- 1systems/wv/configuration.nix
+++ 1systems/wv/configuration.nix
@@ -1,10 +1,12 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, modulesPath, pkgs, ... }:
 
 {
   imports = [
     ../../2configs/plasma-desktop
     ../../2configs/standard-filesystems.nix
     ../../2configs/hardware/intel.nix
+
+    (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
   nix.maxJobs = 4;
@@ -28,7 +30,11 @@
   system.stateVersion = "21.11";
 
   boot = {
-    initrd.luks.devices.crypto.device = "/dev/nvme0n1p4";
+    initrd = {
+      availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+      kernelModules = [ "dm-snapshot" ];
+      luks.devices.crypto.device = "/dev/nvme0n1p4";
+    };
     loader = {
       systemd-boot.enable = false;
       efi.canTouchEfiVariables = false;
