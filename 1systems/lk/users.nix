bfcd5aaa342dd4fe8eda36e57d107c657646efa9
Enno Richter <enno.richter@fraam.de>
2022-05-16 17:13:10 +0200

    lk: configure nix-direnv

--- 1systems/lk/users.nix
+++ 1systems/lk/users.nix
@@ -23,5 +23,10 @@
       userEmail = "liam.koehn@fraam.de";
       userName = "Liam Köhn";
     };
+
+    programs.direnv.enable = true;
+    programs.direnv.nix-direnv.enable = true;
+    # optional for nix flakes support in home-manager 21.11, not required in home-manager unstable or 22.05
+    programs.direnv.nix-direnv.enableFlakes = true;
   };
 }
