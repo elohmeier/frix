f04c95b9db27e602c8bcb89af825457bb0fe071e
Enno Richter <enno.richter@fraam.de>
2022-05-29 22:51:54 +0200

    burpsuite-pro: 2021.10 -> 2022.3.8

--- 5pkgs/burpsuite-pro/default.nix
+++ 5pkgs/burpsuite-pro/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   pname = "burpsuite";
-  version = "2021.10";
+  version = "2022.3.8";
 
   src = fetchurl {
     name = "burpsuite.jar";
@@ -10,7 +10,7 @@ stdenv.mkDerivation rec {
       "https://portswigger.net/Burp/Releases/Download?product=pro&version=${version}&type=Jar"
       "https://web.archive.org/web/https://portswigger.net/Burp/Releases/Download?product=pro&version=${version}&type=Jar"
     ];
-    sha256 = "sha256-rVSSqN/oQ7++TQNbIKJ2A/hfSyAFcd9h6Aj8Um6zHe8=";
+    sha256 = "sha256-Q/8cFJrWhZisxe5X5WXURvbqaq0KhLpjBSLRpyK62kI=";
   };
 
   dontUnpack = true;
