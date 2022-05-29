34640924a0811d0073d216c702c1e693adb1f872
Enno Richter <enno.richter@fraam.de>
2022-05-29 22:51:27 +0200

    matrix: configure go-neb alertmanager

--- 1systems/matrix/modules/go-neb.nix
+++ 1systems/matrix/modules/go-neb.nix
@@ -7,19 +7,51 @@
     bindAddress = "0.0.0.0:${toString config.frix.ports.go-neb}";
     baseUrl = "http://matrix.pug-coho.ts.net:4050";
 
+    # use `https matrix.fraam.de/_matrix/client/r0/login identifier[type]=m.id.user identifier[user]=<<USERNAME>> password=<<PASSWORD>> type=m.login.password` to get a token
     config = {
-      clients = [{
-        UserID = "@pm-bot:fraam.de";
-        AccessToken = "$PM_BOT_ACCESS_TOKEN";
-        DeviceID = "$PM_BOT_DEVICE_ID";
-        HomeserverURL = "https://matrix.fraam.de";
-        Sync = true;
-        AutoJoinRooms = true;
-        DisplayName = "pm-bot";
-        AcceptVerificationFromUsers = [ ":fraam.de" ];
-      }];
+      clients = [
+        {
+          UserID = "@alertmanager-bot:fraam.de";
+          AccessToken = "$ALERTMANAGER_BOT_ACCESS_TOKEN";
+          DeviceID = "$ALERTMANAGER_BOT_DEVICE_ID";
+          HomeserverURL = "https://matrix.fraam.de";
+          Sync = true;
+          AutoJoinRooms = true;
+          DisplayName = "alertmanager-bot";
+          AcceptVerificationFromUsers = [ ":fraam.de" ];
+        }
+        {
+          UserID = "@pm-bot:fraam.de";
+          AccessToken = "$PM_BOT_ACCESS_TOKEN";
+          DeviceID = "$PM_BOT_DEVICE_ID";
+          HomeserverURL = "https://matrix.fraam.de";
+          Sync = true;
+          AutoJoinRooms = true;
+          DisplayName = "pm-bot";
+          AcceptVerificationFromUsers = [ ":fraam.de" ];
+        }
+      ];
 
       services = [
+        {
+          ID = "alertmanager_service";
+          Type = "alertmanager";
+          UserID = "@alertmanager-bot:fraam.de";
+          Config = {
+            webhook_url = "http://matrix.pug-coho.ts.net:4050/services/hooks/YWxlcnRtYW5hZ2VyX3NlcnZpY2U";
+            rooms = {
+              # Enno
+              "!ITyZhYetpXJHZTcgtl:fraam.de" = {
+                text_template = "{{range .Alerts -}} [{{ .Status }}] {{index .Labels \"alertname\" }}: {{index .Annotations \"description\"}} {{ end -}}";
+                # html_template = ''
+		# TODO
+                # '';
+                msg_type = "m.text";
+              };
+            };
+          };
+        }
+
         {
           ID = "kanboard_service";
           Type = "kanboard";
