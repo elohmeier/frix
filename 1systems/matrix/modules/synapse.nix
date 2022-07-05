c7550c3d66f18b25a4b733e490f3ad87079c3578
Enno Richter <enno.richter@fraam.de>
2022-07-05 20:50:31 +0200

    matrix: 22.05 config update

--- 1systems/matrix/modules/synapse.nix
+++ 1systems/matrix/modules/synapse.nix
@@ -10,76 +10,72 @@ in
   services.matrix-synapse = {
     enable = true;
 
-    server_name = serverName;
-    public_baseurl = "https://${fqdn}/";
+    settings = {
+      server_name = serverName;
+      public_baseurl = "https://${fqdn}/";
 
-    database_type = "psycopg2";
-    database_args.dbname = "synapse";
+      database = {
+        name = "psycopg2";
+        args.database = "synapse";
+      };
 
-    listeners = [
-      {
-        port = config.frix.ports.matrix-synapse;
-        bind_address = "127.0.0.1";
-        type = "http";
-        tls = false;
-        x_forwarded = true;
-        resources = [
-          {
-            names = [ "client" "federation" ];
-            compress = false;
-          }
+      listeners = [
+        {
+          port = config.frix.ports.matrix-synapse;
+          bind_addresses = [ "127.0.0.1" ];
+          type = "http";
+          tls = false;
+          x_forwarded = true;
+          resources = [
+            {
+              names = [ "client" "federation" ];
+              compress = false;
+            }
+          ];
+        }
+      ];
+
+      enable_set_displayname = false;
+      enable_set_avatar_url = false;
+      enable_3pid_changes = false;
+
+      auto_join_rooms = [ "#alle:fraam.de" ];
+
+      autocreate_auto_join_rooms = true;
+      autocreate_auto_join_rooms_federated = false;
+
+      email = {
+        smtp_host = "smtp-relay.gmail.com";
+        smtp_port = 587;
+        enable_tls = true;
+        notif_from = "fraam chat <matrix@fraam.de>";
+        client_base_url = "https://chat.fraam.de/";
+        app_name = "fraam chat";
+        enable_notifs = true;
+      };
+
+      password_config = {
+        enabled = true;
+        policy = { enabled = true; minimum_length = 12; };
+      };
+
+      retention = {
+        enabled = true;
+        default_policy = { min_lifetime = "1d"; max_lifetime = "120d"; };
+        purge_jobs = [
+          { longest_max_lifetime = "3d"; interval = "1d"; }
+          { shortest_max_lifetime = "3d"; interval = "1d"; }
         ];
-      }
-    ];
+      };
 
-    extraConfig = ''
-      enable_set_displayname: false
-      enable_set_avatar_url: false
-      enable_3pid_changes: false
+      federation_domain_whitelist = [ serverName "matrix.org" "nerdworks.de" ];
 
-      auto_join_rooms:
-        - "#alle:fraam.de"
-
-      autocreate_auto_join_rooms: true
-      autocreate_auto_join_rooms_federated: false
-
-      email:
-        smtp_host: smtp-relay.gmail.com
-        smtp_port: 587
-        enable_tls: true
-        notif_from: "fraam chat <matrix@fraam.de>"
-        client_base_url: "https://chat.fraam.de/"
-        app_name: fraam chat
-        enable_notifs: true
-      
-      password_config:
-        enabled: true
-        policy:
-          enabled: true
-          minimum_length: 12
-
-      retention:
-        enabled: true
-        default_policy:
-          min_lifetime: 1d
-          max_lifetime: 120d
-        purge_jobs:
-          - longest_max_lifetime: 3d
-            interval: 1d
-          - shortest_max_lifetime: 3d
-            interval: 1d
-      
-      federation_domain_whitelist:
-        - ${serverName}
-        - matrix.org
-        - nerdworks.de
-    '';
-
-    turn_uris = [
-      "turn:turn.fraam.de:3478?transport=udp"
-      "turn:turn.fraam.de:3478?transport=tcp"
-    ];
-    turn_user_lifetime = "1h";
+      turn_uris = [
+        "turn:turn.fraam.de:3478?transport=udp"
+        "turn:turn.fraam.de:3478?transport=tcp"
+      ];
+      turn_user_lifetime = "1h";
+    };
 
     extraConfigFiles = [
       config.frix.secrets."synapse-oidc.yml".path
